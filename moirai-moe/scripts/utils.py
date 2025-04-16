b048336f981b1ed5140e67aa8f788150d332ebeb
anamabo <anamabo3@gmail.com>
2025-04-16 12:04:13 +0200

    Update plot function in utils

--- moirai-moe/scripts/utils.py
+++ moirai-moe/scripts/utils.py
@@ -1,12 +1,11 @@
 from matplotlib import pyplot as plt
-import matplotlib.ticker as mtick
 import pandas as pd
 
 
 def plot_time_series(table: pd.DataFrame, title: str, figsize=(8, 6)):
     fig = plt.figure(figsize=figsize)
 
-    plt.plot(table["date"].values, (table["target"] / table["target"].max()).values)
+    plt.plot(table["date"].values, table["target"].values)
     plt.xlabel('date', size=16)
     plt.ylabel('Value', size=16)
 
@@ -15,11 +14,6 @@ def plot_time_series(table: pd.DataFrame, title: str, figsize=(8, 6)):
 
     # Rotate x-tick labels
     plt.xticks(rotation=45)
-
-    # Format y-axis to show numbers in trillions with text to the right
-    plt.gca().yaxis.set_major_formatter(mtick.ScalarFormatter(useOffset=True))
-    plt.gca().yaxis.offsetText.set_fontsize(10)
-    plt.gca().yaxis.offsetText.set_x(1.07)
 
     # Make plot's frame invisible
     plt.gca().spines['top'].set_visible(False)
